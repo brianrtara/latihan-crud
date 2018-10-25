a326d47ae1d58f08c864e029957d33d7fa2ae6c0
brianrtara <brianrtara@gmail.com>
2018-10-25 19:57:32 +0700

    first commit

---
+++ app/about.php
@@ -0,0 +1,8 @@
+<?php
+    $pageTitle = "About";
+    $nama = "Ala Rai";
+    $tgl_lahir = "28 Juni 1998";
+    $tempat_lahir = "Subang";
+    $pekerjaan = "Mahasiswa";
+    $hobby = "Tidur, Makan, Ngoding";
+?>
---
+++ template/navbar.php
@@ -0,0 +1,10 @@
+<div class="navbar navbar-default">
+    <a class="navbar-brand" href="#">Title</a>
+    <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php?page=about">About</a></li>
+        <li><a href="index.php?page=contactme">Contact</a></li>
+        <li><a href="index.php?page=siswa">siswa</a></li>
+        <li><a href="index.php?page=contact">contact</a></li>
+    </ul>
+</div>
